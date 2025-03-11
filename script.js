6b7f6a020839c41b6e266960e2504900d3757ced
joeycheung001 <joeycheung001@gmail.com>
2025-03-11 01:00:14 -0400

    navbar interactive functions

---
+++ script.js
@@ -0,0 +1,13 @@
+function navDropdown() {
+    var x = document.getElementById("dropdown-menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+window.onresize = function() {
+    var x = document.getElementById("dropdown-menu");
+    x.style.display = "block";
+}
