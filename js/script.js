56b2dfc66167bfba9a7042bee854d1cda4464c86
joeycheung001 <joeycheung001@gmail.com>
2025-05-04 18:29:35 -0400

    hamburger menu html and animate

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-
 function starAudio() {
     var starSound = document.getElementById('sound-star');
     starSound.play();
 }
 
 window.onresize = function() {
-    var x = document.getElementById("dropdown-menu");
+    const dropdown = document.getElementById("dropdown-menu");
     var divWidth = window.innerWidth;
 
     if (divWidth<=700) {
-        x.style.display = "none";
+        dropdown.style.display = "none";
+        bar1.style.rotate = "0deg";
+        bar2.style.opacity = "100";
+        bar3.style.rotate = "0deg";
     }else{
-        x.style.display = "block";
+        dropdown.style.display = "block";
     }
 }
 
 function navDropdown() {
-    var x = document.getElementById("dropdown-menu");
-    if (x.style.display === "none") {
-		x.style.display = "block";
+    const dropdown = document.getElementById("dropdown-menu");
+    const bar1 = document.getElementById("bar1");
+    const bar2 = document.getElementById("bar2");
+    const bar3 = document.getElementById("bar3");
+
+    if (dropdown.style.display === "none") {
+		dropdown.style.display = "block";
+        bar1.style.rotate = "-45deg";
+        bar2.style.opacity = "0";
+        bar3.style.rotate = "45deg";
 	} else {
-		x.style.display = "none";
+		dropdown.style.display = "none";
+        bar1.style.rotate = "0deg";
+        bar2.style.opacity = "100";
+        bar3.style.rotate = "0deg";
 	}
 }
